4b76cea6c08c0e4494d64092d575dcbd6e8b99ac
Francesca <francescabrondi@gmail.com>
2020-02-14 16:06:00 +0100

    Improved about section and sidebar

--- src/components/About.js
+++ src/components/About.js
@@ -3,5 +3,26 @@ import React from "react";
 export default () => (
   <div className="About">
     <h1>About me</h1>
+    <div className="Presentation">
+      <img
+        src="https://scontent.fmxp6-1.fna.fbcdn.net/v/t1.0-9/72414762_2736260529757899_2412866447404433408_n.jpg?_nc_cat=105&_nc_ohc=bFekWYWXmdUAX_UcgKH&_nc_ht=scontent.fmxp6-1.fna&oh=9f8b97439f98a87399ead96fc499fd2c&oe=5F035D1B"
+        alt=""
+      />
+      <div className="text">
+        <h4>Francesca Brondi</h4>
+        <i class="fas fa-map-marked-alt">
+          {" "}
+          <span> Milan, Italy </span>
+        </i>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+          molestias necessitatibus aliquid quidem, neque modi? Qui deleniti fuga
+          odio quae. Enim id praesentium nemo consequuntur non provident esse
+          accusantium officia, nulla quo dolore dolorem fugiat sit eum dolorum,
+          rem exercitationem architecto corrupti sunt similique iusto vero
+          expedita aliquid. Ex, illum.
+        </p>
+      </div>
+    </div>
   </div>
 );
